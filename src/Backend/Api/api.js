07a6019d458a4e79e028751ec03953c54baa1dec
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-04-04 02:28:47 +0400

    change backend-api url

--- src/Backend/Api/api.js
+++ src/Backend/Api/api.js
@@ -1,9 +1,1 @@
-export const apiURL = "https://shopery-idp1.onrender.com/api/v1";
-
-// apiUrlProd = "https://shopery-api-dc140f8d48a7.herokuapp.com/";
-
-//  apiUrlDev =  "https://shopery-api-staging-61f06384c4d8.herokuapp.com/api/v1";
-
-// apiUrlVerify = "https://shopery-api-staging-61f06384c4d8.herokuapp.com/api/v1/auth/verify"
-
-// apiURLresendCode  = "https://shopery-api-staging-61f06384c4d8.herokuapp.com/api/v1/auth/resend-code"
+export const apiURL = "https://shopery-tmyx.onrender.com/api/v1";
